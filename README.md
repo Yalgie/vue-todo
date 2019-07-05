4ede874d31ebb0318c23adf408e21414b88e42b8
Daniel Yalg <daniel@thecodingforge.com.au>
2019-07-05 20:18:07 +1000

    finishing basic todo app with CR_D didn't do update (U)

--- README.md
+++ README.md
@@ -3,3 +3,44 @@
 - Using vue-cli to install a hello world application.
 - Assuming default babel es-lint for install.
 - Installed vue chrome dev tools
+- Building a basic todo app forst to understand vue a bit better
+- Using axios instead of Fetch API 
+- Pulling dummy data from json placeholder
+- Using uuid library to generate random ids for key bindings on new todos
+
+
+
+
+
+
+
+
+
+
+# Task 
+
+Create a single page application that displays 'Next to go’ races using our API. 
+
+A user should see 5 races at all times, and they should be sorted by time ascending. Race should disappear from the list after 1 min past the start time (advertised_start). 
+
+User should see meeting name (meeting_name), race number (race_number) and countdown timer that indicates the start of the race. 
+
+User should be able to toggle race categories to view races belonging to only the selected category. 
+
+Categories are defined by IDs and are the following. 
+- Greyhound racing: category_id: '9daef0d7-bf3c-4f50-921d-8e818c60fe61' 
+- Harness racing: category_id: '161d9be2-e909-4326-8c2c-35ed71fb460b' 
+- Horse racing: category_id: '4a2788f8-e825-4d36-9894-efd4baf1cfae' 
+
+## Requirements 
+- Use vue.js framework (https://vuejs.org/) 
+- Use Neds API to fetch a list of races 
+```
+GET https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=10
+Content-type: application/json 
+```
+
+## Optional 
+- Use vuex 
+- Unit tests 
+- Documentation 
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,46 @@
 <template>
     <div id="app">
+        <Header />
         <img alt="Vue logo" src="./assets/neds-logo.png">
+        <AddTodo v-on:add-todo="addTodo" />
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
     </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import axios from 'axios';
+
+import Header from './components/layout/Header.vue';
+
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
 
 export default {
     name: 'app',
     components: {
-        Todos
+        Todos,
+        Header,
+        AddTodo
     },
     data() {
         return {
-            todos: [{
-                id: 1,
-                title: "Title 1",
-                completed: false
-            }, {
-                id: 2,
-                title: "Title 2",
-                completed: false
-            }, {
-                id: 3,
-                title: "Title 3",
-                completed: false
-            }]
+            todos: []
         }
     },
     methods: {
         deleteTodo(id) {
             this.todos = this.todos.filter(todo => todo.id !== id);
+        },
+        addTodo(newTodo) {
+            this.todos = [...this.todos, newTodo];
         }
+    },
+    created() {
+        axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5").then((res) => {
+            this.todos = res.data;
+        }).catch((e) => {
+            throw e;
+        });
     }
 }
 </script>
